5b6e9d881c406637ca5bdee2635682009bae52c5
ciaranRoche <ciaranroche@hotmail.co.uk>
2019-03-03 15:25:37 +0000

    add http server and env var

--- r3x.py
+++ r3x.py
@@ -1,7 +1,15 @@
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 import SocketServer
+import os
+import logging
+
+PORT = os.environ.get("PORT", None)
 
 def execute(r3xFunc):
+    if PORT == None:
+        print("PORT environment variable was not set, r3x exit")
+        return
+
     class RubiXServer(BaseHTTPRequestHandler):        
         def do_POST(self):
             self.send_response(200)
@@ -9,7 +17,7 @@ def execute(r3xFunc):
             self.end_headers()
             self.wfile.write(r3xFunc())
         
-    def run(server_class=HTTPServer, handler_class=RubiXServer, port=8080):
+    def run(server_class=HTTPServer, handler_class=RubiXServer, port=int(PORT)):
         server_address = ('', port)
         httpd = server_class(server_address, handler_class)
         print 'Starting httpd...'
